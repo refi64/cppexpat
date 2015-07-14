4c2f75bb71f0957f403f48dce782c4285a8f819d
Ryan Gonzalez <rymg19@gmail.com>
2015-07-14 12:06:46 -0500

    Tweak the examples

--- ex1.cpp
+++ ex1.cpp
@@ -1,10 +1,9 @@
-// Include some headers
 #include <cppexpat.hpp>
 #include <iostream>
 #include <string>
 
-// CppExpat classes are in the cppexpat namespace
-// All parsers should derive from the cppexpat::ParserBase base class
+// CppExpat classes are in the cppexpat namespace.
+// All parsers should derive from the cppexpat::ParserBase base class.
 class MyDerivedParser : public cppexpat::ParserBase {
     // ElementAttr is an alias for std::map<std::string, std::string>.
     // It is a map of an element's attributes.
@@ -12,23 +11,20 @@ class MyDerivedParser : public cppexpat::ParserBase {
     void start(std::string name, cppexpat::ElementAttr attr) {
         std::cout << "start: " << name << std::endl;
     }
-
     // This is called when an element ends.
     void end(std::string name) {
         std::cout << "end: " << name << std::endl;
     }
-
     // This is called for character data.
     void chardata(std::string data) {
         std::cout << "character data: " << data << std::endl;
     }
-
     // This is for processing instructions.
     void pinstr(std::string, std::string) {}
 };
 
 int main() {
-    MyDerivedParser p; // Create an instance of the parser class
-    p.parse(R"(<x><a b="c">abc</a></x>)"); // Parse a string
+    MyDerivedParser p; // Create an instance of the parser class.
+    p.parse(R"(<x><a b="c">abc</a></x>)"); // Parse a string.
     return 0;
 }
--- ex2.cpp
+++ ex2.cpp
@@ -4,9 +4,9 @@
 
 int main() {
     /* XMLParser lets you simply specify callbacks instead of creating a parser
-       class. It's useful if you're only going to parse once or twice. */
+       class. It's useful if you're only going to construct a parser once. */
     cppexpat::XMLParser p;
-    // If this syntax is foreign to you, look up C++11's lambdas
+    // If this syntax is foreign to you, look up C++11's lambdas.
     p.set_start_handler([=](std::string s, cppexpat::ElementAttr attr) {
         std::cout << "start: " << s << std::endl;
     });
